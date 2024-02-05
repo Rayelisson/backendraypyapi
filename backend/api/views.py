7bc4224c2eb0233125fc81ff237387a3fc05b85f
rayelisson <33360322+Rayelisson@users.noreply.github.com>
2024-02-05 20:45:21 -0300

    parte do dia 05/02/24

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,17 +4,17 @@ from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-
 from products.models import Product
+from products.serializers import ProductSerializer
 
 
-@api_view(["GET"])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by('?').first()
-    data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return Response(data)
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # data = serializer.save()
+        print(serializer.data)
+        return Response({"invalid": "not good data"}, status=400)
 
 
 # Create your views here.
